e415ad8618de1c9f7f84f52a1a16a535d5637031
Daksh-Singh5 <dakshsingh6612@gmail.com>
2025-02-22 19:37:20 +0530

    first commit

---
+++ index.py
@@ -0,0 +1,8 @@
+num = [1,2,3,4,5,6,7,8,9,10]
+
+def square(num):
+    return num**2
+
+squarenum = list(map(square,num))
+
+print(squarenum)
---
+++ index1.py
@@ -0,0 +1,4 @@
+set1 = {"a","b" , "c" , "d" , "e"}
+set2 = {1,2,3,4,5}
+zipset = list(zip(set1,set2))
+print(zipset)
